b961ce0a6a96f112c0f4df8caac7bf8919a796b6
Jonathan Shapiro <shapiroj18@gmail.com>
2020-12-02 18:50:39 -0500

    testing heroku config vars

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+import os
 from flask import Flask
+
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 # app.config.from_pyfile('config.py')
 
 @app.route("/")
 def hello():
-    return "hello world"
+    return "hello world", f"{os.environ.get('TEST')"
 
 @app.route("/<name>")
 def hello_name(name):
