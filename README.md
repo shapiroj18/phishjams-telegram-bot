d7261d0e6efc3cb5a1986d237206e09623aa5b6b
Jonathan Shapiro <jonathanshapiro@Jonathans-MBP.lan1>
2020-12-02 18:04:57 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Phish Telegram Bot
+
+Commands:
+1. `PHISH <YEAR>` returns a random phish jam from that year
---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+
+@app.route("/")
+def hello():
+    return "hello world"
+
+@app.route("/<name>")
+def hello_name(name):
+    return f"hello {name}"
+
+if __name__ == '__main__':
+    app.run()
