d71d39ee4d986a126d89650da70bc464d4f60ef4
Jonathan Shapiro <shapiroj18@gmail.com>
2020-12-03 23:38:30 -0500

    testing

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def index():
 
 @app.route("/phish")
 def phish():
-    return render_template("base.html")
+    return 'ok' # render_template("base.html")
 
 @app.route(f"/{auth_key}", methods=["POST"])
 def respond():
