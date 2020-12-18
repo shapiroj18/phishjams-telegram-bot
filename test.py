d46e34a0d905c0643db21c5c9a46e6a07bc97f23
Jonathan Shapiro <shapiroj18@gmail.com>
2020-12-18 18:18:13 -0500

    testing port func

--- test.py
+++ test.py
@@ -19,12 +19,12 @@ def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
     
-def help_command(update: Update, context: CallbackContext) -> None:
+def help_command(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
+def echo(update, context):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
 
