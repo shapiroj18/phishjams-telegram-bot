fc48e59f3f77e89ccf440e4e45a301efd5861506
Jonathan Shapiro <shapiroj18@gmail.com>
2020-12-19 12:52:21 -0500

    working towards daily func

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def send_logo(update, context):
 def random_jam(context):
     """Sends daily jam"""
     job = context.job
-    context.bot.send_audio(job.context, audio='https://phish.in/audio/000/020/111/20111.mp3')  
+    context.bot.send_audio(job.context, audio='http://phish.in/audio/000/020/111/20111.mp3')  
     # song= "Sand"
     # date = "1999-12-13"
     # response = phishin_api.get_song_url(song=song, date=date)
