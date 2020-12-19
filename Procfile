933d7f736616d9c800e2efe6151af3d20146b372
Jonathan Shapiro <shapiroj18@gmail.com>
2020-12-19 11:22:35 -0500

    flip flop

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b :$PORT app:app
+web: python3 app.py
--- app.py
+++ app.py
@@ -1,26 +1,30 @@
 import os
-import telegram
-import re
+import logging
 from time import sleep
 import telegram
 from telegram.ext import (
-    Dispatcher,
+    Updater,
     CommandHandler,
     MessageHandler,
     Filters,
-    CallbackContext,
 )
-
-from flask import Flask, request, render_template
 
 from phish_bot.phishnet_api import PhishNetAPI
 from phish_bot.phishin_api import PhishINAPI
 
-url = os.environ.get("URL")
 auth_key = os.environ.get("BOT_TOKEN")
+heroku_app_name = os.environ.get('HEROKU_APP_NAME')
 
 phishnet_api = PhishNetAPI()
 phishin_api = PhishINAPI()
+
+# Enable Logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+PORT = int(os.environ.get('PORT', '8443'))
 
 # Start message
 def start(update, context):
@@ -84,29 +88,15 @@ def unset(update, context):
     text = "Jam successfully cancelled" if job_removed else "You have no active Jam"
     update.message.reply_text(text)
     
-            
-
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return "."
-
-
-@app.route("/phish")
-def phish():
-    return render_template("phish.html")
-
-
-@app.route(f"/{auth_key}", methods=["POST"])
-def respond():
+def error(update, context):
+    """Log errors caused by updates"""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
     
+     
+def main():
     # initialize bot
-    bot = telegram.Bot(token=auth_key)
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-    dispatcher = Dispatcher(bot, None)
+    updater = Updater(auth_key, use_context=True)
+    dispatcher = updater.dispatcher
     
     # handlers
     dispatcher.add_handler(CommandHandler("start", start))
@@ -114,8 +104,47 @@ def respond():
     dispatcher.add_handler(CommandHandler("random_jam", random_jam))
     dispatcher.add_handler(CommandHandler("unset", unset))
     
-    # process an update
-    dispatcher.process_update(update)
+    # error handler
+    dispatcher.add_error_handler(error)
+    
+    # Start bot
+    updater.start_webhook(listen = "0.0.0.0", port=PORT, url_path=auth_key)
+    updater.set_webhook(heroku_app_name + auth_key)
+    
+    
+if __name__ == '__main__':
+    main()    
+
+
+# app = Flask(__name__)
+
+
+# @app.route("/")
+# def index():
+#     return "."
+
+
+# @app.route("/phish")
+# def phish():
+#     return render_template("phish.html")
+
+
+# @app.route(f"/{auth_key}", methods=["POST"])
+# def respond():
+    
+#     # initialize bot
+#     bot = telegram.Bot(token=auth_key)
+#     update = telegram.Update.de_json(request.get_json(force=True), bot)
+#     dispatcher = Dispatcher(bot, None)
+    
+#     # handlers
+#     dispatcher.add_handler(CommandHandler("start", start))
+#     dispatcher.add_handler(CommandHandler("logo", send_logo))
+#     dispatcher.add_handler(CommandHandler("random_jam", random_jam))
+#     dispatcher.add_handler(CommandHandler("unset", unset))
+    
+#     # process an update
+#     dispatcher.process_update(update)
     
     
     
@@ -124,116 +153,116 @@ def respond():
     
 
 
-    chat_id = update.message.chat.id
-    msg_id = update.message.message_id
+#     chat_id = update.message.chat.id
+#     msg_id = update.message.message_id
 
-    text = update.message.text.encode("utf-8").decode().lower()
+#     text = update.message.text.encode("utf-8").decode().lower()
 
-    print("got message: ", text)
+#     print("got message: ", text)
 
-    if text == "/startasdf":
-        bot_welcome = welcome_message
-        bot.send_message(
-            chat_id=chat_id,
-            text=bot_welcome,
-            parse_mode="Markdown",
-            reply_to_message_id=msg_id,
-        )
+#     if text == "/startasdf":
+#         bot_welcome = welcome_message
+#         bot.send_message(
+#             chat_id=chat_id,
+#             text=bot_welcome,
+#             parse_mode="Markdown",
+#             reply_to_message_id=msg_id,
+#         )
 
-    elif text == "logasdfasdo":
-        logo_url = "http://4.bp.blogspot.com/_2CnQWIZQ3NY/SoDbSGrZnxI/AAAAAAAABVQ/tZ6OTg-AzyM/s320/phi.jpg"
-        bot.send_photo(chat_id=chat_id, photo=logo_url, reply_to_message_id=msg_id)
+#     elif text == "logasdfasdo":
+#         logo_url = "http://4.bp.blogspot.com/_2CnQWIZQ3NY/SoDbSGrZnxI/AAAAAAAABVQ/tZ6OTg-AzyM/s320/phi.jpg"
+#         bot.send_photo(chat_id=chat_id, photo=logo_url, reply_to_message_id=msg_id)
 
-    elif text.startswith("mp3"):
+#     elif text.startswith("mp3"):
 
-        # text must be of the format "/mp3 YYYY-MM-DD song_name"
-        parsed_text = text.split(", ")
-        if len(parsed_text) == 3:
-            response = phishin_api.get_song_url(parsed_text[1], parsed_text[2])
-            if response.startswith("http"):
-                links_text = f""" \
-                Find info for the show at [phish.net]({phishnet_api.get_show_url(parsed_text[2])})\n\
-                Find audio for the full show at [phish.in](phish.in/{parsed_text[2]})\
-                """
-                bot.send_chat_action(chat_id=chat_id, action="typing")
-                bot.send_message(
-                    chat_id=chat_id,
-                    text=links_text,
-                    parse_mode="Markdown",
-                    reply_to_message_id=msg_id,
-                )
-                bot.send_audio(chat_id=chat_id, audio=response)
-            else:
-                bot.send_message(
-                    chat_id=chat_id,
-                    text=response,
-                    parse_mode="Markdown",
-                    reply_to_message_id=msg_id,
-                )
-        else:
-            response = "The command must look like \n`mp3, song name, YYYY-MM-DD`"
-            bot.send_message(
-                chat_id=chat_id,
-                text=response,
-                parse_mode="Markdown",
-                reply_to_message_id=msg_id,
-            )
+#         # text must be of the format "/mp3 YYYY-MM-DD song_name"
+#         parsed_text = text.split(", ")
+#         if len(parsed_text) == 3:
+#             response = phishin_api.get_song_url(parsed_text[1], parsed_text[2])
+#             if response.startswith("http"):
+#                 links_text = f""" \
+#                 Find info for the show at [phish.net]({phishnet_api.get_show_url(parsed_text[2])})\n\
+#                 Find audio for the full show at [phish.in](phish.in/{parsed_text[2]})\
+#                 """
+#                 bot.send_chat_action(chat_id=chat_id, action="typing")
+#                 bot.send_message(
+#                     chat_id=chat_id,
+#                     text=links_text,
+#                     parse_mode="Markdown",
+#                     reply_to_message_id=msg_id,
+#                 )
+#                 bot.send_audio(chat_id=chat_id, audio=response)
+#             else:
+#                 bot.send_message(
+#                     chat_id=chat_id,
+#                     text=response,
+#                     parse_mode="Markdown",
+#                     reply_to_message_id=msg_id,
+#                 )
+#         else:
+#             response = "The command must look like \n`mp3, song name, YYYY-MM-DD`"
+#             bot.send_message(
+#                 chat_id=chat_id,
+#                 text=response,
+#                 parse_mode="Markdown",
+#                 reply_to_message_id=msg_id,
+#             )
 
-    elif text == "random":
-        # should be able to enter random or year or song name
-        # lookup jam chart
-        # random date in jam chart
-        # send info about show on phish.net and relisten and/or phish.in link to song
-        # for relisten: get url from inspect > network (refresh page) > year (YYYY-MM-DD) > source_id (maybe in sources['review_count']['sets']['source_id'])
-        response = phishnet_api.get_jamchart_songs()
-        relisten_url = "https://relisten.net/phish/1991/12/04/david-bowie?source=162594"
-        message = f"Your song is {response[1]}"
-        audio_url = "https://phish.in/audio/000/031/671/31671.mp3"
-        caption = "Ya Mar 1999-03-05"
+#     elif text == "random":
+#         # should be able to enter random or year or song name
+#         # lookup jam chart
+#         # random date in jam chart
+#         # send info about show on phish.net and relisten and/or phish.in link to song
+#         # for relisten: get url from inspect > network (refresh page) > year (YYYY-MM-DD) > source_id (maybe in sources['review_count']['sets']['source_id'])
+#         response = phishnet_api.get_jamchart_songs()
+#         relisten_url = "https://relisten.net/phish/1991/12/04/david-bowie?source=162594"
+#         message = f"Your song is {response[1]}"
+#         audio_url = "https://phish.in/audio/000/031/671/31671.mp3"
+#         caption = "Ya Mar 1999-03-05"
 
-        bot.send_message(chat_id=chat_id, text=message, reply_to_message_id=msg_id)
-        bot.send_audio(chat_id=chat_id, audio=audio_url, caption=caption)
+#         bot.send_message(chat_id=chat_id, text=message, reply_to_message_id=msg_id)
+#         bot.send_audio(chat_id=chat_id, audio=audio_url, caption=caption)
 
-    elif text == "sponsor":
+#     elif text == "sponsor":
 
-        sponsorship_text = """ \
-        If you want to support the development of this project, please consider [contributing!](https://github.com/sponsors/shapiroj18)!
-        """
+#         sponsorship_text = """ \
+#         If you want to support the development of this project, please consider [contributing!](https://github.com/sponsors/shapiroj18)!
+#         """
 
-        bot.send_message(
-            chat_id=chat_id,
-            text=sponsorship_text,
-            parse_mode="Markdown",
-            reply_to_message_id=msg_id,
-        )
+#         bot.send_message(
+#             chat_id=chat_id,
+#             text=sponsorship_text,
+#             parse_mode="Markdown",
+#             reply_to_message_id=msg_id,
+#         )
 
-    else:
-        try:
-            text = re.sub(r"/W", "_", text)
-            bot.send_message(
-                chat_id=chat_id,
-                text="Not a phishable command \U0001F420",
-                reply_to_message_id=msg_id,
-            )
-        except Exception:
-            bot.send_message(
-                chat_id=chat_id,
-                text="There was a problem in the name you used, please use a different name",
-                reply_to_message_id=msg_id,
-            )
+#     else:
+#         try:
+#             text = re.sub(r"/W", "_", text)
+#             bot.send_message(
+#                 chat_id=chat_id,
+#                 text="Not a phishable command \U0001F420",
+#                 reply_to_message_id=msg_id,
+#             )
+#         except Exception:
+#             bot.send_message(
+#                 chat_id=chat_id,
+#                 text="There was a problem in the name you used, please use a different name",
+#                 reply_to_message_id=msg_id,
+#             )
 
-    return "ok"
-
-
-@app.route("/setwebhook", methods=["GET", "POST"])
-def set_webhook():
-    s = bot.setWebhook(f"{url}{auth_key}")
-
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
+#     return "ok"
 
 
-if __name__ == "__main__":
-    app.run(threaded=True)
+# @app.route("/setwebhook", methods=["GET", "POST"])
+# def set_webhook():
+#     s = bot.setWebhook(f"{url}{auth_key}")
+
+#     if s:
+#         return "webhook setup ok"
+#     else:
+#         return "webhook setup failed"
+
+
+# if __name__ == "__main__":
+#     app.run(threaded=True)
