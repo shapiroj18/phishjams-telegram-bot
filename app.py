688a1d6380cb7d5e98a73b9538013c5307a1fa55
Jonathan Shapiro <shapiroj18@gmail.com>
2020-12-18 19:11:50 -0500

    added func

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import os
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-from phish_bot.phishnet_api import PhishNetAPI
-from phish_bot.phishin_api import PhishINAPI
+# from phish_bot.phishnet_api import PhishNetAPI
+# from phish_bot.phishin_api import PhishINAPI
 
 auth_key = os.environ.get("BOT_TOKEN")
 app_name = os.environ.get("HEROKU_APP_NAME")
@@ -20,7 +20,14 @@ logger = logging.getLogger(__name__)
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    welcome_message = """
+    \U0001F420 Welcome to the Phish Bot!
+
+    See commands below!
+    `logo`: returns the classic rainbow logo
+    `mp3, song, YYYY-MM-DD`: returns the audio of a track on a specific date 
+    """
+    context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_message)
     
 def help_command(update, context):
     """Send a message when the command /help is issued."""
@@ -29,7 +36,11 @@ def help_command(update, context):
 
 def echo(update, context):
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Not a very phishy command \U0001F420. Type "/help" if you need some ideas.')
+
 
 
 def main():
@@ -40,9 +51,10 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
     
     # on noncommand i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
     
     PORT = os.environ.get('PORT')
     
