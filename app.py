2e0e63f91d4746fc1f14d2302ed4f5ce515cc032
Jonathan Shapiro <shapiroj18@gmail.com>
2020-12-19 12:40:40 -0500

    working towards daily func

--- app.py
+++ app.py
@@ -58,9 +58,8 @@ def random_jam(context):
     response = phishin_api.get_song_url(song=song, date=date)
     if response.startswith("http"):
                 links_text = f"""[Show Info]({phishnet_api.get_show_url(date)})\n[Show Audio](phish.in/{date})"""
-                
+    context.bot.send_audio(jobs.context, audio=response)       
     context.bot.send_message(job.context, text=links_text, parse_mode = "Markdown")
-    context.bot.send_audio(jobs.context, audio=response)
 
 
 def remove_job_if_exists(name, context):
