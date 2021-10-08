30701423ad40366ec9c2d28b3097e27fa007b295
Jonathan Shapiro <shapiroj18@gmail.com>
2021-10-08 12:27:33 -0400

    added basic tests for pytest

---
+++ tests/test_commands_messages.py
@@ -0,0 +1,24 @@
+import os
+from commands.messages import Messages
+
+messages = Messages()
+
+def test_start_message():
+    assert messages.start_message() == f"""\U0001F420 Welcome to the Phish Bot! Send /features for bot commands!"""
+    
+def test_features_message():
+    heroku_flask_url = os.getenv("HEROKU_FLASK_URL")
+    assert messages.features_message() == f"""
+        You can send me commands like:
+        /queue (let's you add a random or specific jam to the online player: {heroku_flask_url})
+        /randomjam (sends a random Phish jam)
+        /subscribedailyjam (random daily jam emails)
+        /unsubscribedailyjam (remove daily jam emails)
+        /subscribemjm (reminder when mystery jam monday is posted)
+        /unsubscribemjm (remove MJM reminders)
+        /code (links to code repositories and contributing)
+        /help (help menu)
+        """
+
+def test_help_message():
+    assert messages.help_message() == f"""Type /features for full bot commands."""
